76708fb07fcb1a5f1e24ed33f0c6e79d0d212132
Upendra Kristipati <upendrakristipati@users.noreply.github.com>
2026-05-12 21:54:21 +0530

    Initial commit for metaarch api gateway

---
+++ src/main/java/com/metaarch/apigateway/MetaArchApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.metaarch.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaArchApiGatewayApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MetaArchApiGatewayApplication.class, args);
+  }
+}
